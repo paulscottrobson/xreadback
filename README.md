dd699722495fd369030b4efe97ecdb4d448bf258
Paul Robson <paul@robsons.org.uk>
2020-08-04 09:40:49 +0100

    Update

--- README.md
+++ README.md
@@ -1,17 +1,10 @@
 # xreadback
 
-Currently in development 
-========================
-
-An attempt to sort out the XPlane 11 default ATC Readback issue
-
-The issue being the absurd requirement to click readback messages with the mouse. Unless
-you are close to ambidextrous that means you have to let go of the joystick in order to grab
-the damn mouse to move and click it. 
-
-However good the coders of XPlane may be, this is a remarkably stupid UI issue when most of the
-messages are simply echoing. If they can't allocate a function to it, there should be an option to 
-make readbacks automatic. 
+An attempt to sort out the XPlane 11 default ATC Readback issue, whereby you have to click readback 
+messages with the mouse. Unless you are close to ambidextrous that means you have to let go of the 
+joystick in order to grab the mouse to move and click it. For rookies like me flying by hand this is
+often a problem. (It is a mystery why this has not been fixed. The default ATC isn't great, but this
+makes it so much less irritating).
 
 Installation
 ============
@@ -25,7 +18,8 @@ Python3 programming language
 	(from https://www.python.org/downloads/)
 	When installing python click the checkbox to 'add to PATH'
 
-Open a command prompt (text input window) and install the following Python libraries.
+Open a command prompt and install the following Python libraries.
+
 Each should display some progress information and end with "Successfully installed" or
 something like that.
 
@@ -39,12 +33,14 @@ sdl2 library
 pynput Python library
 	(Install via "pip install pynput")
 
+If you get errors try adding a -U "pip install -U pynput"
+
 Usage
 =====
 
-The app is very simple. It scans the joystick buttons requested and when one is pressed it 
-moves the mouse cursor to a specific position and clicks it. It can do this for as many 
-places on the display as you like. 
+The app is very simple. Keyboard presses and Joystick button presses are checked against a
+list. If a match is found, the mouse cursor is moved to a position on the screen and the left
+mouse button is clicked.
 
 (The obvious target is the top menu entry on ATC, which is sometimes "Readback Transmission")
 
@@ -55,27 +51,53 @@ Probably it is best to anchor it to a side. I tend to have it at the bottom left
 
 The app is run at a terminal or command prompt.
 
-To identify the buttons and get the position, do :
+To see the keyboard and joystick button names try the following:
 
-python xreadback.py test
+python xreadback.py events
 
-This is incredibly rough and ready but shows you where the mouse cursor is, and what buttons 
-are being pressed. (Interrupt it with control C)
+When a button or key is pressed it will show ** Fired Event 'xxxx' ** where xxxx is the name
+of the event, e.g. k:x is the x key, and b:1 is button #1. (Interrupt it with control C)
+
+Joystick buttons do not seem to work on complex setups. I don't know why. Changing 
+SDL_JoystickOpen(0) currently at line 54 to different numbers might help, but we haven't 
+managed to get this to work.
+
+To see the mouse position try the following
+
+python xreadback.py mouse
+
+This just prints the current mouse position in the window, as you move it around you will see
+the numbers change. Move it to where you want to click and make a note of that screen position.
 
 Now suppose you want button 4 to do the readback, and the centre of the readback button is at
 146,810. You would run the app with :
 
-python xreadback.py 4,146,810
+python xreadback.py b:4,146,810
 
 This makes button 4 move the mouse cursor to 146,810 and click it. It will click whatever is at that
 location. It's really for in flight, where you may not want to take your hands off the smaller planes 
 controls at the time, it's especially irritating when it's just to acknowledge a change of heading.
 
+To make the space bar do the same thing
+
+python xreadback.py k:space,146,810
+
+Just leave it running in the background while you run X-Plane. It shouldn't take up many resources.
+
 Notes: 
 
 1) these are physical screen positions, so if you run it in Windowed mode and move it about it will 
-depend where the window is.
-2) the command prompt 
+depend where the window is. The top left of the screen is 0,0
 
-Paul Robson, July 2020
+2) The install is not trivial. However, this is a good thing. This program is effectively what is called
+a "key logger". It monitors what keys you press. The problem with such programs is that you could modify it
+to write all the keys you press to a file, which mean passwords could be stolen as you type them in.
 
+Because you can "see" the program in its original programmer form, it is possible to check this does not
+do this.
+
+Paul Robson, August 2020
+paul@robsons.org.uk
+
+Many thanks to Mark Smolen who helped with Windows testing and several attempts to try to make it work on
+complex setups.
--- xreadback.py
+++ xreadback.py
@@ -3,8 +3,8 @@
 #
 #		Name: 		xreadback.py
 #		Author:		Paul Robson (paul@robsons.org.uk)
-#		Purpose:	Convert joystick button presses to GUI mouse clicks
-#		Date:		31th July 2020
+#		Purpose:	Convert joystick button presses to GUI mouse clicks (3rd version !)
+#		Date:		4th August 2020
 #
 # ********************************************************************************************
 # ********************************************************************************************
@@ -18,7 +18,7 @@ from pynput import *
 
 # ********************************************************************************************
 #
-#									A wrapper for pynput 
+#								A wrapper for pynput mouse controller
 #
 # ********************************************************************************************
 
@@ -58,7 +58,7 @@ class EventSources(object):
 	#		Initialise SDL and Joystick subsystem
 	#
 	def initialiseSDL(self):
-		SDL_Init(SDL_INIT_JOYSTICK)
+		SDL_Init(SDL_INIT_JOYSTICK)													# start SDL, only Joystick.
 		EventSources.isInitialised = True
 	#
 	#		Close down.
@@ -74,16 +74,16 @@ class EventSources(object):
 	def run(self,buttonHandlerList = []): 						
 		assert EventSources.isInitialised											# check running SDL
 		running = True							
-		event = SDL_Event()
-		self.listener = keyboard.Listener(
-    		on_press=self.on_press)
+		event = SDL_Event()															# events stored here
+		self.listener = keyboard.Listener(	 										# on_press called for every
+    		on_press=self.on_press)													# key press
 		self.listener.start()
 		while running:																# we *might* break out but probably won't.
 			while SDL_PollEvent(ctypes.byref(event)) != 0: 							# don't seem to have button-down events.
 				if event.type == SDL_QUIT:
 					running = False
 				if event.type == SDL_JOYBUTTONDOWN:									# pass on button events.
-					self.eventHandler("B:"+str(event.jbutton.button))
+					self.eventHandler("b:"+str(event.jbutton.button))
 				#
 			time.sleep(0.1)															# check 10 times a second, don't go mad.
 	#
@@ -91,17 +91,113 @@ class EventSources(object):
 	#			
 	def on_press(self,key):
 		try:
-			ch = key.char
+			ch = key.char 															# the character
 		except AttributeError:
-			ch = str(key)
-			ch = ch[4:] if ch.startswith("Key.") else ch
-		self.eventHandler("K:"+ch.lower())
+			ch = str(key) 															# if it isn't one convert
+			ch = ch[4:] if ch.startswith("Key.") else ch 							# to key name e.g. "shift"
+		self.eventHandler("k:"+ch.lower()) 											
 	#
 	#		Dummy event handler
 	#
 	def eventHandler(self,evType):
 		print("** Fired Event '{0}' **".format(evType))
 
+# ********************************************************************************************
+#
+#					Event source class that actually runs the triggers
+#
+# ********************************************************************************************
+
+class ExecEventSources(EventSources):
+	#
+	#		Subclass run to store event handlers
+	#
+	def run(self,eventHandlers):					
+		self.eventHandlers = {} 													# save triggers to do
+		for eh in eventHandlers:													# convert to a lookup dictionary
+			self.eventHandlers[eh.getButton()] = eh
+		EventSources.run(self)														# call superclass	
+	#
+	#		Override event handler
+	#
+	def eventHandler(self,evType):
+		evType = evType.lower()
+		if evType in self.eventHandlers:											# do we know this one ?
+			print("Firing event ",evType)
+			self.eventHandlers[evType].fireButton()									# yes, go do it.
+
+# ********************************************************************************************
+#
+#									Test Handler class
+#
+# ********************************************************************************************
+
+class ButtonHandlerTest(object):
+	def __init__(self,button):
+		self.button = button
+	def getButton(self):
+		return self.button
+	def fireButton(self):
+		print("Fired ",self.button)
+
+# ********************************************************************************************
+#
+#							Actual move/click handler class
+#
+# ********************************************************************************************
+
+class ClickButtonHandler(object):
+	def __init__(self,definition,mouseController):
+		self.mouseController = mouseController
+		if isinstance(definition,str):
+			definition = self.makeList(definition)
+		assert len(definition) == 3,"Bad mouse/button definition"
+		print("Added definition Button {0} at {1},{2}".format(definition[0],definition[1],definition[2]))
+		self.button = definition[0]
+		self.x = definition[1]
+		self.y = definition[2]
+	#
+	#		Get the button ID
+	#
+	def getButton(self):
+		return self.button
+	#
+	#		Handle button being clicked.
+	#
+	def fireButton(self):
+		print("Firing "+str(self.button))
+		self.mouseController.move(self.x,self.y)
+		self.mouseController.click()
+	#
+	#		Convert string list to list of integers, mouse definition
+	#
+	def makeList(self,defn):
+		defn = defn.replace(" ","").replace("\t"," ")
+		m = re.match("^(.*?)\\,(\\d+)\\,(\\d+)$",defn.strip())
+		assert m is not None,"Bad definition "+defn
+		return [m.group(1).strip().lower(),int(m.group(2)),int(m.group(3))]
+
 EventSources.isInitialised = False 
 
-EventSources().run()
+if len(sys.argv) == 1:
+	print("xreadback v0.3 by Paul Robson paul@robsons.org.uk. MIT Licensed.")
+	print("\txreadback events - display key presses/joystick buttons as pressed")
+	print("\txreadback mouse - display mouse cursor position")
+	print("\txreadback event,x,y event,x,y - map button/key event to click at x,y ")
+	sys.exit(0)
+
+mouseCtrl = MouseController()
+
+if len(sys.argv) == 2:
+	cmd = sys.argv[1].lower()
+	if cmd == "mouse":
+		while True:
+			print("Mouse position: ",mouseCtrl.getPosition())
+	if cmd == "events":
+		EventSources().run()
+
+eventHandlers = []
+for eDesc in sys.argv[1:]:
+	eventHandlers.append(ClickButtonHandler(eDesc,mouseCtrl))
+
+ExecEventSources().run(eventHandlers)
